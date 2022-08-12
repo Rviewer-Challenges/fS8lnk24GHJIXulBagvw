df76116b3bc44054f3acd6a3ea0364b1a2dfe2c0
Diana Hernandez <dianahdezsoler@gmail.com>
2022-08-12 20:42:45 +0200

    Initial Commit

---
+++ MIChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MIChat
+//
+//  Created by Diana Hernández on 12/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MIChat/MIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIChatApp.swift
+//  MIChat
+//
+//  Created by Diana Hernández on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
